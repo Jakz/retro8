6c2e1a71672dd55f88a751d93b519f3818b90a09
Jack <jack@pixbits.com>
2021-03-12 16:02:22 +0100

    added funkey key mapping

--- src/config.h
+++ src/config.h
@@ -7,7 +7,9 @@
 
 #define SOUND_ENABLED true
 
-#if defined(__LIBRETRO__)
+#if defined(FUNKEY_S)
+#define PLATFORM PLATFORM_FUNKEY
+#elif defined(__LIBRETRO__)
 #define PLATFORM PLATFORM_LIBRETRO
 #elif defined(_WIN32)
 #define PLATFORM PLATFORM_WIN32
@@ -75,6 +77,26 @@ static constexpr int SCREEN_HEIGHT = 240;
 
     static constexpr auto KEY_MENU = SDLK_RETURN;
     static constexpr auto KEY_EXIT = SDLK_ESCAPE;
+
+  #elif PLATFORM == PLATFORM_FUNKEY
+
+    static constexpr auto KEY_UP = SDLK_u;
+    static constexpr auto KEY_DOWN = SDLK_d;
+    static constexpr auto KEY_LEFT = SDLK_l;
+    static constexpr auto KEY_RIGHT = SDLK_r;
+
+    static constexpr auto KEY_ACTION1_1 = SDLK_b; // A
+    static constexpr auto KEY_ACTION2_1 = SDLK_a; // B
+    static constexpr auto KEY_ACTION1_2 = SDLK_y; // Y
+    static constexpr auto KEY_ACTION2_2 = SDLK_x; // X
+
+    static constexpr auto KEY_MUTE = SDLK_UNKNOWN;
+    static constexpr auto KEY_PAUSE = SDLK_UNKNOWN;
+
+    static constexpr auto KEY_NEXT_SCALER = SDLK_h; // L
+
+    static constexpr auto KEY_MENU = SDLK_s;
+    static constexpr auto KEY_EXIT = SDLK_UNKNOWN;
 
   #endif
 
